09c276aede55045d9071cf65ae67365b9e1f1b4b
Dariusz Gorski <dariuszgoorski@gmail.com>
2024-01-21 16:49:23 +0100

    added shield

---
+++ scenes/PlayerScene/player.gd
@@ -0,0 +1,125 @@
+extends RigidBody2D
+signal lives_changed
+signal dead
+signal shield_changed
+
+@export var engine_power: int
+@export var spin_power = 8000
+@export var bullet_scene: PackedScene
+@export var fire_rate = 0.25
+@export var max_shield = 100.0
+@export var shield_regen = 5.0
+
+enum {INIT, ALIVE, INVULNERABLE, DEAD}
+
+var can_shoot = true
+var thrust = Vector2.ZERO
+var rotation_dir = 0
+var screensize = Vector2.ZERO
+var state = INIT
+var reset_pos = false
+
+var shield = 0: set = set_shield
+var lives = 0: set = set_lives
+
+func set_lives(value):
+	shield = max_shield
+	lives = value
+	lives_changed.emit(lives)
+	if lives <= 0:
+		change_state(DEAD)
+	else:
+		change_state(INVULNERABLE)
+		
+func set_shield(value):
+	value = min(value, max_shield)
+	shield = value
+	shield_changed.emit(shield/max_shield)
+	if shield <= 0:
+		lives -= 1
+		explode()
+
+func _ready():
+	change_state(ALIVE)
+	$GunCooldown.wait_time = fire_rate
+	screensize = get_viewport_rect().size
+	
+func _process(delta):
+	shield += shield_regen * delta
+	get_input()
+	
+func get_input():
+	if state in [DEAD, INIT]:
+		thrust = Vector2.ZERO
+	if Input.is_action_pressed("thrust"):
+		thrust = transform.x * engine_power
+	if Input.is_action_pressed("shoot") and can_shoot:
+		shoot()
+	rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+
+func _physics_process(delta):
+	constant_force = thrust
+	constant_torque = rotation_dir * spin_power
+	
+func _integrate_forces(physics_state):
+	var xform = physics_state.transform
+	xform.origin.x = wrapf(xform.origin.x, 0, screensize.x)
+	xform.origin.y = wrapf(xform.origin.y, 0, screensize.y)
+	physics_state.transform = xform
+	if reset_pos:
+		physics_state.transform.origin = screensize / 2
+		reset_pos = false
+	
+func change_state(new_state):
+		match new_state:
+			INIT:
+				$CollisionShape2D.set_deferred("disabled", true)
+				$Sprite2D.modulate.a = 0.5
+			ALIVE:
+				$CollisionShape2D.set_deferred("disabled", false)
+				$Sprite2D.modulate.a = 1
+			INVULNERABLE:
+				$CollisionShape2D.set_deferred("disabled", true)
+				$Sprite2D.modulate.a = 0.5
+				$InvulnerabilityTimer.start()
+			DEAD:
+				$CollisionShape2D.set_deferred("disabled", true)
+				$Sprite2D.hide()
+				linear_velocity = Vector2.ZERO
+				dead.emit()
+		state = new_state
+
+func shoot():
+	if state == INVULNERABLE:
+		return
+	can_shoot = false
+	$GunCooldown.start()
+	var bullet = bullet_scene.instantiate()
+	get_tree().root.add_child(bullet)
+	bullet.start($Muzzle.global_transform)
+
+func reset():
+	reset_pos = true
+	$Sprite2D.show()
+	lives = 3
+	change_state(ALIVE)
+
+func _on_gun_cooldown_timeout():
+	can_shoot = true
+
+
+func _on_invulnerability_timer_timeout():
+	change_state(ALIVE)
+
+
+
+func _on_body_entered(body):
+	if body.is_in_group("rocks"):
+		shield -= body.size * 25
+		body.explode()
+		
+func explode():
+	$Explosion.show()
+	$Explosion/AnimationPlayer.play("explosion")
+	await $Explosion/AnimationPlayer.animation_finished
+	$Explosion.hide()
---
+++ scenes/main.gd
@@ -0,0 +1,81 @@
+extends Node
+@export var rock_scene: PackedScene
+@export var enemy_scene: PackedScene
+var screensize: Vector2
+var level = 0
+var score = 0
+var playing = false
+
+func _ready():
+	screensize = get_viewport().get_visible_rect().size
+	$Player/Sprite2D.hide()
+	
+func _process(delta):
+	if not playing:
+		return
+	if get_tree().get_nodes_in_group("rocks").size() == 0:
+		new_level()
+		
+func new_game():
+	get_tree().call_group("rocks", "queue_free")
+	level = 0
+	score = 0
+	$HUD.update_score(score)
+	$HUD.show_message("Get ready!")
+	$Player.reset()
+	await $HUD/Timer.timeout
+	playing = true
+	
+func new_level():
+	level += 1
+	$HUD.show_message("Wave %s" % level)
+	for i in level:
+		spawn_rock(3)
+		
+	$EnemyTimer.start(randf_range(5,10))
+
+func spawn_rock(rock_size: int, pos=null, vel=null):
+	if pos == null:
+		$RockPath/RockSpawn.progress = randi()
+		pos = $RockPath/RockSpawn.position
+	if vel == null:
+		vel = Vector2.RIGHT.rotated(randf_range(0, TAU)) * randf_range(50,125)
+	var rock = rock_scene.instantiate()
+	rock.screensize = screensize
+	rock.start(pos,vel,rock_size)
+	call_deferred("add_child", rock)
+	rock.exploded.connect(self._on_rock_exploded)
+	
+func _on_rock_exploded(size,radius,pos, vel):
+	if size <= 1:
+		return
+	for offset in [-1,1]:
+		var dir = $Player.position.direction_to(pos).orthogonal() * offset
+		var newpos = pos + dir * radius
+		var newvel = dir * vel.length() * 1.1
+		spawn_rock(size - 1,newpos, newvel)
+		
+func game_over():
+	playing = false
+	$HUD.game_over()
+	
+func _input(event):
+	if event.is_action_pressed("pause"):
+		if not playing:
+			return
+		get_tree().paused = not get_tree().paused
+		var message = $HUD/VBoxContainer/Message
+		if get_tree().paused:
+			message.text = "Paused"
+			message.show()
+		else:
+			message.hide()
+			message.text = ""
+
+
+func _on_enemy_timer_timeout():
+	print('enemy spawn')
+	var enemy = enemy_scene.instantiate()
+	add_child(enemy)
+	enemy.target = $Player
+	$EnemyTimer.start(randf_range(20,40))
